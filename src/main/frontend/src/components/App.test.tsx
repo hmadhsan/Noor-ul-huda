3171a8aaebf0a5d038e7ac60a1fec77c6a001b01
Hasnain Javed <Hasnain.Javed@nha.org.au>
2021-09-11 16:03:05 +1000

    Added todo tests for mobile navigation (need figuring out)

--- src/main/frontend/src/components/App.test.tsx
+++ src/main/frontend/src/components/App.test.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { screen } from "@testing-library/react"
 import { render } from "../test-utils"
 import { App } from "./App"
@@ -7,4 +6,39 @@ test.skip("renders learn react link", () => {
   render(<App />)
   const linkElement = screen.getByText(/learn chakra/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+describe("mobile navigation", () => {
+  test.todo("should open when clicked")
+  /**
+   , async () => {
+    Object.defineProperty(window, "innerWidth", {
+      writable: true,
+      configurable: true,
+      value: 767
+    })
+
+    window.dispatchEvent(new Event("resize"))
+
+    render(<App />, renderOptions)
+
+    userEvent.click(screen.getByRole("button", { name: /open menu/i }))
+
+    expect(screen.getByText(/close menu/i)).toBeInTheDocument()
+  }
+   */
+
+  test.todo("should close when clicked")
+  /**
+   , async () => {
+    window.resizeTo(767, 645)
+
+    render(<App />)
+
+    userEvent.click(screen.getByRole("button", { name: /open menu/i }))
+    userEvent.click(screen.getByRole("button", { name: /close menu/i }))
+
+    expect(screen.getByText(/open menu/i)).toBeInTheDocument()
+  }
+   */
 })
