fda6543143c00c264a4958f393aa34e8a3c372d5
Hasnain Javed <Hasnain.Javed@nha.org.au>
2021-10-10 15:02:17 +1100

    Replaced tajweed level drop down with text area

--- src/main/frontend/src/components/enrolment/tajweed/modal/StateSelect.tsx
+++ src/main/frontend/src/components/enrolment/tajweed/modal/StateSelect.tsx
@@ -1,17 +1,41 @@
 import { FormControl, FormLabel, Select, SelectProps } from "@chakra-ui/react"
 
-export const StateSelect = (props: SelectProps) => (
-  <FormControl id="state">
-    <FormLabel>State</FormLabel>
-    <Select maxW="2xs" {...props}>
-      <option>Victoria</option>
-      <option>New South Wales</option>
-      <option>Australian Capital Territory</option>
-      <option>Western Australia</option>
-      <option>Queensland</option>
-      <option>Northern Territory</option>
-      <option>South Australia</option>
-      <option>Tasmania</option>
-    </Select>
-  </FormControl>
-)
+interface StateSelectProps extends SelectProps {
+  selectedValue?: string
+}
+
+export const StateSelect = (props: StateSelectProps) => {
+  const { selectedValue: state } = props
+
+  return (
+    <FormControl id="state">
+      <FormLabel>State</FormLabel>
+      <Select maxW="2xs" {...props} value={state}>
+        <option key="vic" value="Victoria">
+          Victoria
+        </option>
+        <option key="nsw" value="New South Wales">
+          New South Wales
+        </option>
+        <option key="act" value="Australian Capital Territory">
+          Australian Capital Territory
+        </option>
+        <option key="wa" value="Western Australia">
+          Western Australia
+        </option>
+        <option key="qld" value="Queensland">
+          Queensland
+        </option>
+        <option key="nt" value="Northern Territory">
+          Northern Territory
+        </option>
+        <option key="sa" value="South Australia">
+          South Australia
+        </option>
+        <option key="tas" value="Tasmania">
+          Tasmania
+        </option>
+      </Select>
+    </FormControl>
+  )
+}
